fe204e0779ec9d1e2381f29126d1024a402dae46
Bin <ybin4548@gmail.com>
2026-02-26 10:55:57 +0900

    Initial Commit

---
+++ MovieBooking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBooking
+//
+//  Created by 손영빈 on 2/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
