4d75885d5ba691e2bc7ee8ede1bdec809c2d44fe
yeseul jang <yyssjj9999@gmail.com>
2026-02-26 17:56:10 +0900

    🚚build: 데이터 받아오기 위한 MovieResponse 구조체 추가

---
+++ MovieBooking/Models/MovieResponse.swift
@@ -0,0 +1,44 @@
+//
+//  MovieResponse.swift
+//  MovieBooking
+//
+//  Created by Yeseul Jang on 2/26/26.
+//
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let adult: Bool
+    let backdropPath: String?
+    let genreIDs: [Int]
+    let id: Int
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let popularity: Double
+    let posterPath: String?
+    let releaseDate: String
+    let title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDs = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
