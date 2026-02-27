a0cee0d91c62b4e6dafae46cd65439d71ee5cb57
Bin <ybin4548@gmail.com>
2026-02-27 17:33:40 +0900

    ✨feat: 상단 네비게이션 hidden

--- MovieBooking/ViewController/SearchViewController.swift
+++ MovieBooking/ViewController/SearchViewController.swift
@@ -24,6 +24,16 @@ class SearchViewController: UIViewController {
         bind()
         viewModel.fetchMovies()
     }
+    // HomeView 접근 시 상단 네비게이션 제거
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    // 다른 페이지로 접근 시 상단 네비게이션 다시 생성
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
 
 extension SearchViewController {
