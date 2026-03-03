94e5f1f9155ed461b598864b28488858f85c1bef
yeseul jang <yyssjj9999@gmail.com>
2026-03-03 19:12:15 +0900

    🚚build: TicketBookingViewController 생성

---
+++ MovieBooking/ViewController/TicketBookingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TicketBookingViewController.swift
+//  MovieBooking
+//
+//  Created by Yeseul Jang on 3/3/26.
+//
+import UIKit
+
+final class TicketBookingViewController: UIViewController {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
