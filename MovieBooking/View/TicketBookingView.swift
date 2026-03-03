f836361c0346f5f213cd361306ab2c9e1f3a326c
yeseul jang <yyssjj9999@gmail.com>
2026-03-03 19:11:01 +0900

    🚚build: TicketBookingView 생성

---
+++ MovieBooking/View/TicketBookingView.swift
@@ -0,0 +1,18 @@
+//
+//  TicketBookingView.swift
+//  MovieBooking
+//
+//  Created by Yeseul Jang on 3/3/26.
+//
+import UIKit
+
+final class TicketBookingView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
