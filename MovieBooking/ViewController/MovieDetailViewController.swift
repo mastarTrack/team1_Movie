fb790cc6f64cb307b18336401e74df83c4104976
Bin <ybin4548@gmail.com>
2026-03-03 17:57:24 +0900

    🐛fix: ViewController -> UIViewController

--- MovieBooking/ViewController/MovieDetailViewController.swift
+++ MovieBooking/ViewController/MovieDetailViewController.swift
@@ -8,7 +8,7 @@ import UIKit
 import SnapKit
 import Kingfisher
 
-final class MovieDetailViewController: ViewController {
+final class MovieDetailViewController: UIViewController {
     private let detailView = MovieDetailView()
     private let viewModel: MovieDetailViewModel
     
