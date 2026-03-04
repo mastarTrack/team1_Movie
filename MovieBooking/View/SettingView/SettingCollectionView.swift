accccab249d135195e354d9ffd34bb99394ac72e
Bin <ybin4548@gmail.com>
2026-03-04 11:25:56 +0900

    ✨feat: CollectionView 생성

---
+++ MovieBooking/View/SettingView/SettingCollectionView.swift
@@ -0,0 +1,42 @@
+//
+//  SettingCollectionView.swift
+//  MovieBooking
+//
+//  Created by 손영빈 on 3/4/26.
+//
+
+import UIKit
+import SnapKit
+
+class SettingCollectionView: UIView {
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SettingCollectionView {
+    private func setup() {
+//        collectionView.isScrollEnabled = false
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+    }
+}
+
+
+extension SettingCollectionView {
+    func createLayout() -> UICollectionViewLayout {
+        var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        config.headerMode = .supplementary
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+}
