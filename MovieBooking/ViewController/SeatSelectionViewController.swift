b495dcfe720c8f3456c7b234dcdf5c0e0fcfba09
yeseul jang <yyssjj9999@gmail.com>
2026-03-05 23:05:06 +0900

    🌟feat: 전에 있던 뷰모델 소유하게 만들기

--- MovieBooking/ViewController/SeatSelectionViewController.swift
+++ MovieBooking/ViewController/SeatSelectionViewController.swift
@@ -9,12 +9,17 @@ import SnapKit
 
 final class SeatSelectionViewController: UIViewController {
     private let seatView = SeatSelectionView()
-    let viewModel = SeatSelectionViewModel()
+    let seatViewModel = SeatSelectionViewModel()
+    private let ticketBookingViewModel: TicketBookingViewModel
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.title = "좌석 선택"
+        
+        seatViewModel.setMaxSelectableCount(
+            ticketBookingViewModel.adultCount + ticketBookingViewModel.childCount
+        )
         
         view.addSubview(seatView)
         seatView.snp.makeConstraints {
@@ -26,30 +31,47 @@ final class SeatSelectionViewController: UIViewController {
         bindView()
     }
     
+    // 아예 뷰모델 넘겨주기
+    init(ticketBookingViewModel: TicketBookingViewModel) {
+        self.ticketBookingViewModel = ticketBookingViewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 예매하기 눌렀을때 좌석 확정되도록 만들기
+    private func didTapSeats() {
+        let seats = seatViewModel.selectedSeatsForSave
+        ticketBookingViewModel.setSelectedSeats(seats)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
     private func setupViewModel() {
-        viewModel.setup(
+        seatViewModel.setup(
             totalSeatsCount: seatView.totalSeatsCount,
             unavailableIndexes: [3,6,100]
         )
-        viewModel.setMaxSelectableCount(4)
     }
     
     // 뷰모델에서 뷰에 받아오는거
     private func bindViewModel() {
         // 시트가 바뀌면 어떤걸 해야하는지
-        viewModel.onSeatStateChanged = { [weak self] index in
+        seatViewModel.onSeatStateChanged = { [weak self] index in
             guard let self else { return }
-            let state = self.viewModel.seatStates[index]
+            let state = self.seatViewModel.seatStates[index]
             self.seatView.updateSeatState(index: index, state: state)
         }
         // 시트가 선택되면 작동할 코드
-        viewModel.onSelectionChanged = { [weak self] titles in
+        seatViewModel.onSelectionChanged = { [weak self] titles in
             self?.seatView.guideLabel.text =
             titles.isEmpty ? "좌석을 선택하세요." :
             "선택: \(titles.joined(separator: " "))"
         }
         // 시트를 제한된 좌석수에 다달았을때
-        viewModel.onLimitReached = { [weak self] max in
+        seatViewModel.onLimitReached = { [weak self] max in
             self?.seatView.guideLabel.text = "\(max)개의 좌석만 선택할 수 있습니다."
         }
     }
@@ -58,21 +80,21 @@ final class SeatSelectionViewController: UIViewController {
     private func bindView() {
         // 뷰에서 시트 눌렸을때 해당 좌석 정보 뷰모델로 보내기
         seatView.onSeatTapped = { [weak self] index, title in
-            self?.viewModel.updateSeatState(index: index, title: title)
+            self?.seatViewModel.updateSeatState(index: index, title: title)
         }
         
-        // 예매하기 버튼 액션 등록
-        seatView.reservationButton.addAction(
-            UIAction { [weak self] _ in
-                self?.didTapReservation()
-            },
-            for: .touchUpInside
-        )
+        //여기서 그냥 저장도 함
+        seatView.reservationButton.addAction(UIAction { [weak self] _ in
+            guard let self else { return }
+            let seats = self.seatViewModel.selectedSeatsForSave
+            self.ticketBookingViewModel.setSelectedSeats(seats)
+            
+            self.ticketBookingViewModel.bookReservation()
+        }, for: .touchUpInside)
     }
     
     private func didTapReservation() {
-        let seats = viewModel.selectedSeatsForSave
+        let seats = seatViewModel.selectedSeatsForSave
         print("저장할 좌석:", seats)
-        // CoreData 저장 로직 VM에서 구현해서 여기로 옮겨야함
     }
 }
