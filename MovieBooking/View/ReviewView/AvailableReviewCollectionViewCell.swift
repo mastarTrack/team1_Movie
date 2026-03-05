aec3c2678c34dbc9ea08097b6899702dbdddaa1b
Bin <ybin4548@gmail.com>
2026-03-05 16:22:21 +0900

    ✨feat: Segment 추가, 클릭 시 셀 변경, 셀 생성

---
+++ MovieBooking/View/ReviewView/AvailableReviewCollectionViewCell.swift
@@ -0,0 +1,104 @@
+//
+//  AvailableReviewCollectionViewCell.swift
+//  MovieBooking
+//
+//  Created by 손영빈 on 3/5/26.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+class AvailableReviewCollectionViewCell: UICollectionViewCell {
+    static let id = "AvailableReviewCollectionViewCell"
+    
+    private let containerView = UIView()
+    private let posterImageView = UIImageView()
+    
+    private let stackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let infoLabel = UILabel()
+    private let watchDateLael = UILabel()
+    
+    private let writeButton = CustomButton(title: "작성")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setAttributes()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AvailableReviewCollectionViewCell {
+    private func setAttributes() {
+        containerView.backgroundColor = .systemGray6
+        containerView.layer.cornerRadius = 20
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        posterImageView.contentMode = .scaleAspectFill
+        posterImageView.layer.cornerRadius = 10
+        posterImageView.clipsToBounds = true
+        posterImageView.backgroundColor = .white
+        
+        titleLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        [infoLabel, watchDateLael].forEach {
+            $0.font = .systemFont(ofSize: 14)
+            $0.textColor = .darkGray
+        }
+        
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillEqually
+        
+    }
+    private func setLayout() {
+        contentView.addSubview(containerView)
+        [titleLabel, infoLabel, watchDateLael].forEach { stackView.addArrangedSubview($0) }
+        [posterImageView, stackView, writeButton].forEach { containerView.addSubview($0) }
+        
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        posterImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview().inset(10)
+            $0.width.equalTo(100)
+            $0.height.equalTo(150)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(10)
+            $0.trailing.equalTo(writeButton.snp.leading).offset(-10)
+            $0.bottom.equalTo(posterImageView.snp.bottom)
+        }
+        
+        writeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-10)
+            $0.width.equalTo(80)
+            $0.height.equalTo(40)
+        }
+    }
+}
+
+extension AvailableReviewCollectionViewCell {
+    func config(data: Reservation) {
+        
+        if let url = URL(string: data.safePosterPath) {
+            posterImageView.kf.setImage(with: url)
+        } else {
+            posterImageView.image = UIImage(systemName: "movieclapper")
+        }
+        
+        titleLabel.text = data.safeTitle
+        infoLabel.text = data.safeTheaterName
+        watchDateLael.text = "\(data.safeWatchDate) 관람"
+        
+    }
+}
--- MovieBooking/ViewController/ReviewViewController.swift
+++ MovieBooking/ViewController/ReviewViewController.swift
@@ -9,7 +9,58 @@ import UIKit
 
 class ReviewViewController: UIViewController {
     
+    private let reviewView = ReviewCollectionView()
+    
+    private var selectedIndex = 0
+    
+    private var availableData: [Reservation] = []
+    
+    override func loadView() {
+        self.view = reviewView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension ReviewViewController {
+    private func setDelegate() {
+        reviewView.delegate = self
+        reviewView.collectionView.delegate = self
+        reviewView.collectionView.dataSource = self
+        
+        reviewView.collectionView.register(ReviewCollectionViewCell.self, forCellWithReuseIdentifier: ReviewCollectionViewCell.id)
+        reviewView.collectionView.register(AvailableReviewCollectionViewCell.self, forCellWithReuseIdentifier: AvailableReviewCollectionViewCell.id)
+    }
+}
+
+extension ReviewViewController: ReviewCollectionViewDelegate {
+    func didChangeSegment(index: Int) {
+        self.selectedIndex = index
+        reviewView.collectionView.reloadData()
+    }
+}
+
+
+extension ReviewViewController: UICollectionViewDelegate {
+    
+}
+
+extension ReviewViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return selectedIndex == 0 ? 1 : availableData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if selectedIndex == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewCollectionViewCell.id, for: indexPath) as? ReviewCollectionViewCell else { return UICollectionViewCell() }
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AvailableReviewCollectionViewCell.id, for: indexPath) as? AvailableReviewCollectionViewCell else { return UICollectionViewCell() }
+            return cell
+        }
+    }
+    
+    
 }
