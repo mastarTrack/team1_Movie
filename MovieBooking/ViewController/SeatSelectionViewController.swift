c890c5f3248b64508eca668f1a16b41bf692672b
yeseul jang <yyssjj9999@gmail.com>
2026-03-05 18:36:18 +0900

    🌟feat: VC에 View 추가하기

--- MovieBooking/ViewController/SeatSelectionViewController.swift
+++ MovieBooking/ViewController/SeatSelectionViewController.swift
@@ -4,4 +4,20 @@
 //
 //  Created by Yeseul Jang on 3/5/26.
 //
+import UIKit
+import SnapKit
 
+final class SeatSelectionViewController: UIViewController {
+    private let seatSelectionView = SeatSelectionView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "좌석 선택"
+
+        view.addSubview(seatSelectionView)
+        seatSelectionView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
